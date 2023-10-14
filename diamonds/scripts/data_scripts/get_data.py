4273ccc8bb8f1edf7e4f16f07a407f652bef5907
arqoofficial <argolubev97@yandex.ru>
2023-10-14 21:36:57 +0300

    NEW: script get_data.py for diamonds.csv download

---
+++ diamonds/scripts/data_scripts/get_data.py
@@ -0,0 +1,42 @@
+import os
+
+from kaggle.api.kaggle_api_extended import KaggleApi
+from loguru import logger
+
+
+def handle_exception(e: Exception, message: str) -> None:
+    error_type = type(e).__name__
+    logger.error(f"An error occurred: {error_type}. {e}")
+    logger.warning(message)
+    raise SystemExit
+
+
+if __name__ == "__main__":
+    logger.info("Start get_data.py script")
+
+    try:
+        api = KaggleApi()
+        api.authenticate()
+        logger.info("Kaggle authentication was succesfull")
+    except Exception as e:
+        handle_exception(
+            e,
+            "You need to provide correct kaggle.json with authentication info",
+        )
+
+    try:
+        data_path = os.path.abspath("../../data/raw/")
+        logger.info("Start Diamonds dataset download")
+        api.dataset_download_files(
+            dataset="shivam2503/diamonds",
+            path=data_path,
+            unzip=True,
+        )
+        logger.success(
+            f"Diamonds dataset was successfully downloaded into `{data_path}`W"
+        )
+    except Exception as e:
+        handle_exception(
+            e,
+            "Something went wrong with kaggle dataset download",
+        )
