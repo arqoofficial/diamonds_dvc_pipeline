db91ee2a0a0af52273b24eafd94da077cb95b9cd
arqoofficial <argolubev97@yandex.ru>
2023-10-15 12:49:52 +0300

    FIX: black and isort fixes

--- scripts/data_scripts/drop_dup_and_na.py
+++ scripts/data_scripts/drop_dup_and_na.py
@@ -1,6 +1,6 @@
-import sys
-import os
 import io
+import os
+import sys
 from pathlib import Path
 
 import pandas as pd
@@ -9,11 +9,11 @@ from loguru import logger
 
 def process_data_pandas(f_input, f_output):
     """Gets csv path, processes df and saves processed csv"""
-    
+
     # read_csv
     df = pd.read_csv(f_input)
     logger.success(f"diamond.csv was loaded from `{f_input}`")
-    
+
     UNNAMED_COLUMN = "Unnamed: 0"
     if UNNAMED_COLUMN in df.columns:
         df.drop(columns=UNNAMED_COLUMN, inplace=True)
@@ -22,7 +22,7 @@ def process_data_pandas(f_input, f_output):
     df.info(buf=buf)
     df_info = "\n".join(buf.getvalue().splitlines()[1:])
     logger.info(f"df_info:\n{df_info}")
-    
+
     # drop_duplicates
     df_shape_before = df.shape
     logger.info(f"df shape before drop_duplicates(): {df_shape_before}")
@@ -30,23 +30,20 @@ def process_data_pandas(f_input, f_output):
     df_shape_after = df.shape
     logger.info(f"df shape after drop_duplicates(): {df_shape_after}")
     logger.success(f"Dropped {df_shape_before[0] - df_shape_after[0]} rows")
-    
+
     # drop_na
     nan_amount = df.isna().sum().sum()
     if nan_amount == 0:
         logger.success("There is no NaN to drop")
     else:
-        logger.warning(
-            f"There is/are {nan_amount} NaN(s), they will be dropped"
-        )
+        logger.warning(f"There is/are {nan_amount} NaN(s), they will be dropped")
         df.dropna(inplace=True)
         logger.info(f"Result df shape: {df.shape}")
 
     # save processed csv
     df.to_csv(f_output, index=False)
     logger.success(
-        "Prepared diamonds.csv without NaN and duplicates "
-        f"was saved as `{f_output}`"
+        "Prepared diamonds.csv without NaN and duplicates " f"was saved as `{f_output}`"
     )
 
 
@@ -60,7 +57,7 @@ def main():
     f_input = os.path.abspath(sys.argv[1])
     if not Path(f_input).exists():
         logger.error(
-            f"diamonds csv not found, please check path correctnes: {f_input}"
+            "diamonds csv not found, please check path correctnes: " f"`{f_input}`"
         )
         raise SystemExit
     f_output = Path(__file__).parents[2] / "data" / "stage1" / "diamonds.csv"
