6df2e898201c74d6d96da8c3193700ff3232bce5
arqoofficial <argolubev97@yandex.ru>
2023-10-17 02:05:34 +0300

    NEW: init model

--- scripts/model_scripts/model_knr.py
+++ scripts/model_scripts/model_knr.py
@@ -1,3 +1,35 @@
+# import os
+# import pickle
+# import sys
+
+# import pandas as pd
+# import yaml
+# from sklearn.tree import DecisionTreeClassifier
+
+# if len(sys.argv) != 3:
+#     sys.stderr.write("Arguments error. Usage:\n")
+#     sys.stderr.write("\tpython dt.py data-file model \n")
+#     sys.exit(1)
+
+# f_input = sys.argv[1]
+# f_output = os.path.join("models", sys.argv[2])
+# os.makedirs(os.path.join("models"), exist_ok=True)
+
+# params = yaml.safe_load(open("params.yaml"))["train"]
+# p_seed = params["seed"]
+# p_max_depth = params["max_depth"]
+
+# df = pd.read_csv(f_input, header=None)
+# X = df.iloc[:, [1, 2, 3]]
+# y = df.iloc[:, 0]
+
+# clf = DecisionTreeClassifier(max_depth=p_max_depth, random_state=p_seed)
+# clf.fit(X, y)
+
+# with open(f_output, "wb") as fd:
+#     pickle.dump(clf, fd)
+
+
 import os
 from pathlib import Path
 
@@ -96,7 +128,8 @@ def main():
 
 
 if __name__ == "__main__":
-    try:
-        main()
-    except Exception as e:
-        handle_exception(e)
+    main()
+    # try:
+    #     main()
+    # except Exception as e:
+    #     handle_exception(e)
